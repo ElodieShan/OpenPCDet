04b229e3dab7906e340b040e32e851bb560d8bf3
elodieshan <ElodieShan@tanway.com>
2020-11-02 10:32:10 +0800

    update

--- tools/scripts/dist_train.sh
+++ tools/scripts/dist_train.sh
@@ -6,8 +6,13 @@ PY_ARGS=${@:2}
 
 # CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch --nproc_per_node=${NGPUS} train.py --launcher pytorch ${PY_ARGS}
 
-CUDA_VISIBLE_DEVICES=1,2 python3 -m torch.distributed.launch --nproc_per_node=2 train.py --launcher pytorch \
---cfg_file cfgs/kitti_models/second_16lines.yaml \
---pretrained_model ../output/kitti_models/second/default/ckpt/checkpoint_epoch_80.pth \
---extra_tag fine_tune_lr0.001 \
---tcp_port 18887
+CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch \
+--nproc_per_node=2 train.py --launcher pytorch \
+--cfg_file cfgs/kitti_models/second.yaml \
+--extra_tag plane_lr0.0015
+
+CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch \
+--nproc_per_node=2 train.py --launcher pytorch \
+--cfg_file cfgs/kitti_models/second2.yaml \
+--extra_tag plane_lr0.003
+
