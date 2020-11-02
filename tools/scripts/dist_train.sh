9fad752ffad7496f009232d124f077ffa0dc9320
elodieshan <ElodieShan@tanway.com>
2020-11-02 21:16:54 +0800

    add KL loss & hint loss

--- tools/scripts/dist_train.sh
+++ tools/scripts/dist_train.sh
@@ -9,10 +9,4 @@ PY_ARGS=${@:2}
 CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch \
 --nproc_per_node=2 train.py --launcher pytorch \
 --cfg_file cfgs/kitti_models/second.yaml \
---extra_tag plane_lr0.0015
-
-CUDA_VISIBLE_DEVICES=0,1 python3 -m torch.distributed.launch \
---nproc_per_node=2 train.py --launcher pytorch \
---cfg_file cfgs/kitti_models/second2.yaml \
---extra_tag plane_lr0.003
-
+--extra_tag test
