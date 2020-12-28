88f366ee7a22d4088904dad97afabea3e8c48d98
elodieshan <ElodieShan@tanway.com>
2020-12-28 15:56:06 +0800

    add sub_loss_weight

--- tools/scripts/dist_train.sh
+++ tools/scripts/dist_train.sh
@@ -14,9 +14,9 @@ PY_ARGS=${@:2}
 # --use_sub_data
 
 CUDA_VISIBLE_DEVICES=1 python3 -m torch.distributed.launch --nproc_per_node=1 train.py \
---launcher pytorch --cfg_file cfgs/kitti_models/second2.yaml \
+--launcher pytorch --cfg_file cfgs/kitti_models/second.yaml \
 --tcp_port 18888 \
---extra_tag test-sub_branch-downchannel \
+--extra_tag test-20201228 \
 --ckpt_save_interval 2 \
 --use_sub_data
 
